32c26811ffeced680a71fba39c600d1508864b3d
gssantaella <gssantaella@gmail.com>
2022-09-19 18:31:50 -0300

    operacoes em array

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 # IMPORTS
 import numpy as np
 from PIL import Image
+
 import os
+import time
 
 
 # CONTANTS
 RES_PATH = 'res'
 IMG_PATH = os.path.join(RES_PATH, 'img')
-IMG_NAME = 'new.png'
+IMG_NAME = 'new1.png'
 
 
 # HELPER
@@ -25,30 +27,35 @@ def main():
     # RENDER
     print(f'\nWIDTH: {IMG_WIDTH}\nHEIGHT: {IMG_HEIGHT}\n')
 
-    pixels = []
-    for j in range(IMG_HEIGHT-1, -1, -1):
-        pixels_row = []
-        for i in range(IMG_WIDTH):
-            r = float(i) / (IMG_WIDTH-1)
-            g = float(j) / (IMG_HEIGHT-1)
-            b = 0.25
+    start = time.time()
+    array = create_image_array(IMG_WIDTH, IMG_HEIGHT)
 
-            ir = int(255.999 * r)
-            ig = int(255.999 * g)
-            ib = int(255.999 * b)
-
-            pixels_row.append((ir,ig,ib))
-        pixels.append(pixels_row)
-
-
-    # create image
-    array = np.array(pixels, dtype=np.uint8)
-    new_image = Image.fromarray(array)
-    new_image_path = os.path.join(IMG_PATH, IMG_NAME)
-    new_image.save(new_image_path)
-    print(f'IMAGE PATH: {new_image_path}\n')
+    end = time.time()
+    print(f'time numpy: {end-start}\n')
+    
+    create_image(array, os.path.join(IMG_PATH, IMG_NAME))
 
     return
+
+def create_image_array(width, height):
+
+    LENGTH = width * height
+
+    xx, yy = np.meshgrid(np.linspace(0, 1, width), np.linspace(1, 0, height))
+    z = np.array([0.25] * LENGTH)
+
+    x = xx.flatten() * 255.999
+    y = yy.flatten() * 255.999
+    z *= 255.999
+
+    return np.stack((x,y,z), axis=1).astype(np.uint8).reshape((height, width, 3))
+
+
+
+def create_image(array, img_path):
+    new_image = Image.fromarray(array)
+    new_image.save(img_path)
+    print(f'IMAGE PATH: {img_path}\n')
 
 
 if __name__ == '__main__':
